9efdefb8072e8b8ff154e12759edef668cda59fe
qfx <qfx@decard.com>
2016-07-14 14:58:21 +0800

    APDU PRINT OK

--- LuaTest/PcscApduTest/PcscApduTestDlg.cpp
+++ LuaTest/PcscApduTest/PcscApduTestDlg.cpp
@@ -70,7 +70,7 @@ END_MESSAGE_MAP()
 
 
 // CPcscApduTestDlg message handlers
-
+CPcscApduTestDlg *g_dlg;
 BOOL CPcscApduTestDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
@@ -101,6 +101,7 @@ BOOL CPcscApduTestDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 
 	// TODO: Add extra initialization here
+	g_dlg = this;
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
@@ -154,7 +155,103 @@ HCURSOR CPcscApduTestDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+void
+__stdcall
+MyHexA(unsigned char* hex, unsigned char* a, int length)
+{
+	unsigned char hLowbit, hHighbit;
+	int i;
+	for (i = 0; i < length * 2; i += 2)
+	{
+		hLowbit = hex[i / 2] & 0x0f;
+		hHighbit = hex[i / 2] / 16;
+		if (hHighbit >= 10)
+			a[i] = hHighbit + '7';
+		else
+			a[i] = hHighbit + '0';
+		if (hLowbit >= 10)
+			a[i + 1] = hLowbit + '7';
+		else
+			a[i + 1] = hLowbit + '0';
+	}
+	a[length * 2] = '\0';
+}
 
+int
+__stdcall
+MyAHex(unsigned char* a, unsigned char* hex, int len)
+{
+	int i;
+	unsigned char aLowbit, aHighbit;
+	unsigned char hconval, lconval;
+	for (i = 0; i < len; i += 2)
+	{
+		aHighbit = toupper(a[i]);
+		if ((aHighbit >= 'A') && (aHighbit <= 'F'))
+			hconval = '7';
+		else if ((aHighbit >= '0') && (aHighbit <= '9'))
+			hconval = '0';
+		else
+			return  -1;
+		aLowbit = toupper(a[i + 1]);
+		if ((aLowbit >= 'A') && (aLowbit <= 'F'))
+			lconval = '7';
+		else if ((aLowbit >= '0') && (aLowbit <= '9'))
+			lconval = '0';
+		else
+			return  -1;
+		hex[(i / 2)] = ((aHighbit - hconval) * 16 + (aLowbit - lconval));
+	}
+	return 0;
+}
+void Printf_Edit(LPVOID lpParam, int controlId, CString AddString)
+{
+	CPcscApduTestDlg *p_dlg;
+	p_dlg = (CPcscApduTestDlg *)lpParam;
+	((CEdit *)p_dlg->GetDlgItem(controlId))->SetSel(p_dlg->GetDlgItem(controlId)->GetWindowTextLength(), p_dlg->GetDlgItem(controlId)->GetWindowTextLength());
+	//((CEdit*)p_dlg->GetDlgItem(controlId))->ReplaceSel(AddString + "\r\n"); 
+	((CEdit*)p_dlg->GetDlgItem(controlId))->ReplaceSel(AddString);
+}
+
+void
+__stdcall
+d_printf_edit(const char* format, ...)
+{
+#ifdef _DEBUG
+	FILE* fp;
+#endif
+	va_list args;
+	char tmp_buffer[4096] = "\0";
+#ifdef _DEBUG
+	fp = fopen("dc.log", "a+");
+#endif
+	va_start(args, format);
+	vsprintf(tmp_buffer, format, args);
+#ifdef _WINDOWS
+	//printf("%s", tmp_buffer);
+	Printf_Edit(g_dlg, IDC_EDIT_RESULT, (CString)tmp_buffer);
+#endif
+#ifdef _DEBUG
+	(void)vfprintf(fp, format, args);
+#endif
+	va_end(args);
+#ifdef _DEBUG
+	fclose(fp);
+#endif
+}
+
+void
+__stdcall
+d_printf_hex_note_edit(const char* note, const char* dat, int len)
+{
+	char log_name[20] = "\0";
+	char log_time[20] = "\0";
+	char tmp_buffer[4096] = "\0";
+	char tmp_buffer2[4096] = "\0";
+
+	MyHexA((unsigned char*)dat, (unsigned char*)tmp_buffer2, len);
+	d_printf_edit("%s(%d):%s\n", note, len, tmp_buffer2);
+}
 
 void CPcscApduTestDlg::OnBnClickedButtonRefresh()
 {
@@ -185,9 +282,9 @@ void CPcscApduTestDlg::OnBnClickedButtonRefresh()
 
 #include <lua.hpp>
 
-int l_ConnectCard(lua_State* luaVM)
+int l_CardApdu(lua_State* luaVM)
 {
-	//const char* pText = luaL_checkstring(luaVM, 1);
+	const char *pApdu = luaL_checkstring(luaVM, 1);
 
 	
 	return 0;
@@ -203,7 +300,7 @@ void CPcscApduTestDlg::OnBnClickedButtonRun()
 	((CComboBox *)GetDlgItem(IDC_COMBO_READER_LIST))->GetLBText(((CComboBox *)GetDlgItem(IDC_COMBO_READER_LIST))->GetCurSel(), str);
 
 	result = SCardConnect(g_sc, str, SCARD_SHARE_SHARED, SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1, &g_sh, &dwActiveProtocol);
-	//d_printf("SCardConnect result : %d\n", result);
+	d_printf_edit("SCardConnect result : %d\n", result);
 	if (result)
 	{
 		return ;
@@ -214,7 +311,7 @@ void CPcscApduTestDlg::OnBnClickedButtonRun()
 	DWORD cByte = SCARD_AUTOALLOCATE;
 
 	result = SCardGetAttrib(g_sh, SCARD_ATTR_ATR_STRING, (LPBYTE)&pbAttr, &cByte);
-	//d_printf("SCardConnect result : %d\n", result);
+	d_printf_edit("SCardGetAttrib result : %d\n", result);
 	if (result)
 	{
 		return;
@@ -229,7 +326,9 @@ void CPcscApduTestDlg::OnBnClickedButtonRun()
 	slen = 20;
 	memcpy(sdat, "\x00\xA4\x04\x00\x0E\x32\x50\x41\x59\x2E\x53\x59\x53\x2E\x44\x44\x46\x30\x31\x00", slen);
 	rlen = 1024;
+	d_printf_hex_note_edit("--->", (char *)sdat, slen);
 	result = SCardTransmit(g_sh, SCARD_PCI_T1, sdat, slen, NULL, rdat, &rlen);
+	d_printf_hex_note_edit("<---", (char *)rdat, rlen);
 
 	result = SCardDisconnect(g_sh, SCARD_LEAVE_CARD);
 
